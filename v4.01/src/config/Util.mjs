7d9e219265fdcda2bc28bdb442ed44ddd12311c8
debiru <main.coeurl@gmail.com>
2025-02-11 12:47:36 +0900

    update src/*

--- v4.01/src/config/Util.mjs
+++ v4.01/src/config/Util.mjs
@@ -16,6 +16,19 @@ const Util = {
       return str.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&');
     },
   },
+  Array: {
+    combine(keys, values) {
+      return Object.fromEntries(Util.Array.zip(keys, values));
+    },
+    zip(...args) {
+      const result = [];
+      const len = Math.max(...args.map((arg) => arg.length));
+      for (let i = 0; i < len; ++i) {
+        result.push([...args.map((arg) => arg[i])]);
+      }
+      return result;
+    },
+  },
   sprintf(format, ...args) {
     let p = 0;
     return format.replace(/%./g, function(m) {
@@ -29,6 +42,22 @@ const Util = {
   },
   rtrim(str, char = '/') {
     return str.replaceAll(new RegExp(Util.sprintf('[%s]+$', Util.RegExp.escape(char)), 'g'), '');
+  },
+  toZippedList(obj) {
+    const args = Object.values(obj);
+    let first = args.shift();
+    if (!Array.isArray(first)) first = [first];
+    args.forEach((arg, idx) => {
+      if (!Array.isArray(arg)) arg = [arg];
+      for (let i = arg.length, k = 0; i < first.length; ++i, ++k) {
+        arg[i] = arg[k];
+      }
+      args[idx] = arg;
+    });
+    args.unshift(first);
+    const keys = Object.keys(obj);
+    const zippedList = Util.Array.zip(...args).map((record) => Util.Array.combine(keys, record));
+    return zippedList;
   },
 };
 

72f80e6fb3df46522a543091a6120290a885e5f4
debiru <main.coeurl@gmail.com>
2025-02-12 09:08:35 +0900

    add myExtGenerateUserInfo

--- v4.01/astro.config.mjs
+++ v4.01/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import myExtIntegration from '/src/config/myExtIntegration';
+import myExtGenerateUserInfo from '/src/config/myExtGenerateUserInfo';
 
 // refs. https://astro.build/config
 export default defineConfig({
   site: 'https://sugamocss.com',
   trailingSlash: 'ignore',
   compressHTML: false,
-  integrations: [myExtIntegration()],
+  integrations: [myExtIntegration(), myExtGenerateUserInfo()],
   build: {
     format: 'file',
   },
